773a43b0b675626777f8d6d73c7a3e36671fc31d
kyliel <kyliel@microsoft.com>
2016-10-30 22:15:41 +0800

    1

--- openshift-origin-rhel/scripts/deployOpenShift.sh
+++ openshift-origin-rhel/scripts/deployOpenShift.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "$*" > /tmp/debug.txt
+whoami >> /tmp/debug.txt
+
 SUDOUSER=$1
 PASSWORD=$2
 PRIVATEKEY=$3
@@ -18,6 +21,8 @@ DOMAIN=$( awk 'NR==2' /etc/resolv.conf | awk '{ print $2 }' )
 
 echo "Generating keys"
 
+runuser -l $SUDOUSER -c "mkdir -p ~/.ssh"
+runuser -l $SUDOUSER -c "chmod 600 ~/.ssh"
 runuser -l $SUDOUSER -c "echo \"$PRIVATEKEY\" > ~/.ssh/id_rsa"
 runuser -l $SUDOUSER -c "chmod 600 ~/.ssh/id_rsa*"
 
